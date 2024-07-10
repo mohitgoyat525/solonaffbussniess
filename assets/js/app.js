c031af17e5c288386bd99f377d4d809df1370935
mohitgoyat525 <montyits71@gmail.com>
2024-07-10 10:05:50 +0530

    add-new-img

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,6 @@ const navbtn = () => {
     document.getElementById("nav-bar").classList.toggle("show-nav")
     document.body.classList.toggle("overflow-hidden")
 }
+<script>
+    AOS.init();
+</script>;
