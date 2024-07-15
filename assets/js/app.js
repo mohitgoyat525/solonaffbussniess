4e7a65b1eba8fec79804665762045e4ec8f8b07e
mohitgoyat525 <montyits71@gmail.com>
2024-07-15 16:16:51 +0530

    add-font-family

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,3 @@ const navbtn = () => {
     document.getElementById("nav-bar").classList.toggle("show-nav")
     document.body.classList.toggle("overflow-hidden")
 }
-<script>
-    AOS.init();
-</script>;
